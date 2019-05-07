472cdc6a574e3cc19cbc6566db7e348752833c75
Huvi Hoberman <huvihoberman@gmail.com>
2019-05-06 21:46:49 -0400

    Initial

---
+++ Candidates.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Candidates.Data
+{
+	public class Class1
+	{
+	}
+}
---
+++ Candidates.Web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Candidates.Web
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
